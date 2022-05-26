2ee3189a06e77eb420f779638b351f6031574fb5
Sam Respass <samrespass@gmail.com>
2022-05-26 18:50:07 -0400

    it can do math now

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,10 +1,43 @@
 let buttons = document.querySelectorAll('.button')
-let results = document.querySelector('.resultScreen')
+let result = document.querySelector('#resultScreen')
+let finisher = document.querySelector('.equate')
+let killer = document.querySelector('.clear')
+let modifier = document.querySelectorAll('.modify')
+
 console.log(buttons)
+console.log(modifier)
+// This is our keypad functionality
 for (let button of buttons) {
     button.addEventListener("click", function (event) {
-        console.log(event.target.innerText);
-        //this'll tell us what number was clicked
-        document.getElementById("resultScreen").innerText += event.target.innerText;
+        result.textContent += event.target.textContent;
+        if (result.textContent.length > 6) {
+            result.textContent = "ERROR";
+        }
+        console.log(event.target.textContent);
     })
 }
+// This is our equal button functionality
+finisher.addEventListener("click", function (formulize) {
+    console.log(eval(result.textContent))
+    result.textContent = eval(result.textContent);
+    if (result.textContent > 999999) {
+        result.textContent = "ERROR";
+    }
+}
+)
+// This is our Modifier Button Functionality
+for (let modify of modifier) {
+    modify.addEventListener("click", function (summer) {
+        result.textContent += summer.target.textContent;
+        console.log(summer.target.textContent);
+        console.log(summer.target.textContent.length);
+        if (result.textContent.length === 1) {
+           modify.disable=true;
+        }
+    })
+}
+// This is our Clear Button Functionality
+killer.addEventListener("click", function (wiper) {
+    result.textContent = "";
+}
+)
